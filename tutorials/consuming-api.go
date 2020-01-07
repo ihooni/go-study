a4c646c68eb8c74a96de60e3e4db23fb2f975e40
ihooni <to@ihooni.com>
2020-01-07 16:19:26 -0500

    tutorial: consuming api

---
+++ tutorials/consuming-api.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	url := "https://api.github.com/users"
+	res, err := http.Get(url)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	defer res.Body.Close()	// for garbage collection
+
+	responseBodyBytes, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	fmt.Println(string(responseBodyBytes))
+}
